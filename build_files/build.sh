23e625163c90f3228bb98fce94246a9a67a3246c
Konstantinos Dichalas <kdihalas@gmail.com>
2025-12-31 21:23:27 +0000

    add 1password

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,14 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+### INSTALL 1PASSWORD ####
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+mkdir -p /opt/1Password
+
+
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y 1password
 
 # Use a COPR Example:
 #
