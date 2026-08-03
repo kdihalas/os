abf5e8a61773c59a2d83cf1fb62f51273f1a8e05
Konstantinos Dichalas <kdihalas@gmail.com>
2026-08-03 15:52:44 +0100

    Add llvm-devel and clang to development libraries installation

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,7 +14,7 @@ dnf5 -y copr enable scottames/ghostty
 dnf5 -y install ghostty
 
 ### INSTAL Development Libraries
-dnf5 -y  group install c-development development-tools
+dnf5 -y  group install c-development development-tools llvm-devel clang
 
 
 ### Remove Ptyxis ###
